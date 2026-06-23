3daf971ed99c3827d03d2ef0b5a52c857ce858b8
olagunju-oluwabukola <olagunjuoluwabukola2018@gmail.com>
2026-06-23 17:40:23 +0100

    scoped

--- client/src/pages/JobTracker.tsx
+++ client/src/pages/JobTracker.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 const JobTracker = () => {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <p className='text-5xl text-purple-500 font-bold' style={{ fontWeight: 'bold' }}>  This feature Will be available soon</p>
     </div>
   )
 }
